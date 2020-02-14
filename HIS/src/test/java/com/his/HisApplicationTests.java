e2d77d2b5e5d4d2fe7b3a19777b913b7fabded47
leonel130 <leonel.rojas.27@unsch.edu.pe>
2020-02-14 12:41:05 -0500

    Model

---
+++ HIS/src/test/java/com/his/HisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.his;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
